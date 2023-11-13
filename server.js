e03f307afc8601c1b9346b2fe30e63ede2e2b265
o.ovcharenko <oleksandr.ovcharenko@depositphotos.com>
2023-11-13 15:29:47 +0200

    Add Node.js server setup

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Serve static files from the root directory
-app.use(express.static('.'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server running on http://localhost:${port}`);
 });
